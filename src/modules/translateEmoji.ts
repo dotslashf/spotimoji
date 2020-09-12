f6142a5f798e7991d5d3c309ef5d46bda09f95e0
fadhlu <fadhlurahman.i@gmail.com>
2020-09-13 00:24:40 +0700

    fix parsing text to emoji method

--- src/modules/translateEmoji.ts
+++ src/modules/translateEmoji.ts
@@ -1,5 +1,9 @@
 const translateEmoji = require('moji-translate');
 const translation = require('translate');
+const checkWord = require('check-word');
+
+const words = checkWord('en');
+
 require('dotenv').config();
 
 translation.engine = 'google';
@@ -7,25 +11,60 @@ translation.key = process.env.TRANSLATE_KEY;
 translation.from = 'id';
 
 export class track2Emoji {
-  trackTitle: String;
-  trackArtist: String;
+  trackTitle: String[];
+  trackArtist: String[];
 
   constructor(track: String, artist: String) {
-    this.trackTitle = track;
-    this.trackArtist = artist;
+    this.trackTitle = track.split(' ');
+    this.trackArtist = artist.split(' ');
   }
 
-  translateToEn(text: String) {
-    return translation(text, { to: 'en' });
+  translateToEn(word: String) {
+    return translation(word, { to: 'en' });
+  }
+
+  isWordinEn(word: String): Boolean {
+    if (words.check(word.toLowerCase)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   async returnEmoji(): Promise<String> {
-    const trackTitle = await this.translateToEn(this.trackTitle);
-    const trackArtist = await this.translateToEn(this.trackArtist);
-    const track = `${translateEmoji.translate(
-      trackTitle,
-      ''
-    )}-${translateEmoji.translate(trackArtist, '')}`;
-    return track;
+    const originalTitle: String = this.trackTitle.join(' ');
+    const originalArtists: String = this.trackArtist.join(' ');
+
+    await Promise.all(
+      this.trackTitle.map(async (word, i) => {
+        if (!this.isWordinEn(word)) {
+          const newWord = await this.translateToEn(word);
+          const emoji = translateEmoji.getEmojiForWord(newWord);
+
+          if (!emoji) {
+            this.trackTitle[i] = word;
+          } else {
+            this.trackTitle[i] = emoji;
+          }
+        }
+      })
+    );
+
+    await Promise.all(
+      this.trackArtist.map(async (word, i) => {
+        const newWord = await this.translateToEn(word);
+        const emoji = translateEmoji.getEmojiForWord(newWord);
+
+        if (!emoji) {
+          this.trackArtist[i] = word;
+        } else {
+          this.trackArtist[i] = emoji;
+        }
+      })
+    );
+
+    return `${this.trackTitle.join(' ')} - ${this.trackArtist.join(
+      ' '
+    )} | ${originalTitle} - ${originalArtists}`;
   }
 }
